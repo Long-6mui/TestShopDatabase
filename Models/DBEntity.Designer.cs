7b1e84cc263a0f35f7bed654b1a7e765c1527c9d
Long 6 mui <thuytinhhoanglong1510@gmail.com>
2025-11-23 13:59:30 +0700

    Recreated clean DB Model and connection string

--- Models/DBEntity.Designer.cs
+++ Models/DBEntity.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Huflit_web\Last final\ShopOnline\ShopOnline\Models\DBEntity.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Huflit_web\TestLastFinal5\Models\DBEntity.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
